82d62b87e22530af4ce82cf2f464cfd14bd64c74
seunghun <kangseunghun9927@gmail.com>
2025-04-30 03:32:03 +0900

    for debug
    Signed-off-by: seunghun <kangseunghun9927@gmail.com>

--- .github/scripts/first-time-contributor.sh
+++ .github/scripts/first-time-contributor.sh
@@ -6,7 +6,11 @@ AUTHOR="$1"
 PR_NUMBER="$2"  
 REPO="$3"  
 GITHUB_TOKEN="$4"  
-  
+
+echo "AUTHOR: $AUTHOR"
+echo "PR_NUMBER: $PR_NUMBER"
+echo "REPO: $REPO"
+
 # Search for merged PRs by this author  
 SEARCH_URL="https://api.github.com/search/issues?q=is:merged+is:pr+author:${AUTHOR}+repo:${REPO}"  
 echo "Checking for merged PRs: ${SEARCH_URL}"  
@@ -19,7 +23,7 @@ COUNT=$(echo "$RESPONSE" | jq -r '.total_count')
   
 if [ "$COUNT" -eq 0 ]; then  
     echo "First-time contributor detected! Adding label..."  
-    curl -s -X POST \  
+    curl -v -X POST \  
         -H "Authorization: token ${GITHUB_TOKEN}" \  
         -H "Accept: application/vnd.github.v3+json" \  
         "https://api.github.com/repos/${REPO}/issues/${PR_NUMBER}/labels" \  
